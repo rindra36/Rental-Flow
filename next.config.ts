adfb8d2a060c0cfa2e41bd24ef5687bb26b41f0f
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2025-12-01 22:55:32 +0300

    feat: add Docker configuration for containerization and a new `/api/ping` health check endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,45 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  devIndicators: {
-    buildActivity: false,
-    appIsrStatus: false,
-  },
-  experimental: {
-    // This is required for the dev server to work in this environment
-    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
-  },
+    output: "standalone",
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+    devIndicators: {
+        buildActivity: false,
+        appIsrStatus: false,
+    },
+    experimental: {
+        // This is required for the dev server to work in this environment
+        allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    },
 };
 
 export default nextConfig;
